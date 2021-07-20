87a3760fce58988a80ce69b291c3266464065e48
Elvira <elya3029@gmail.com>
2021-07-20 09:52:02 +0300

    dynamic_typization.js  to string to number to boolean

---
+++ firstDay/js/dynamic_typization.js
@@ -0,0 +1,67 @@
+"use strict"
+
+// // 1) To String
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
+
+// // 2) Concatanation
+
+// console.log(typeof(5 + ""));
+// console.log(typeof(undefined + ""));
+// console.log(typeof(null + ""));
+// console.log(typeof({} + ""));
+// console.log(typeof([] + ""));
+
+// const num = 5;
+
+// console.log("https://vk.com/catalog/" + num);
+
+// const fontSize = 26 + "px";
+
+
+// // 1) To Number
+
+// console.log(typeof(Number("7")));
+
+// // 2) 
+
+// console.log(typeof(+"7"));
+
+// // 3)
+
+// console.log(typeof(parseInt("15px", 10)));
+// console.log(parseInt("15px", 10));
+
+// let answer = +prompt("??", "");
+
+
+
+// 1) To Boolean
+
+// False : 0; ""; null; undefined; NaN
+// True : else
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log("working..");
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log("working..");
+// }
+
+// console.log(typeof(Boolean("4")));
+// console.log(Boolean("4"));
+
+// // 3)
+// console.log(typeof(!"4"));
+// console.log(!"4");
+
+// console.log(typeof(!!"4"));
+// console.log(!!"4");
+
+
+
